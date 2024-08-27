b7e5b9e71ad63850977ea0c7d8ceec56524fa718
nickk888samp <kevinnickk888@gmail.com>
2024-08-28 00:17:12 +0200

    Added AI steering for testing

--- Assets/Scripts/AI/EnemyAI.cs
+++ Assets/Scripts/AI/EnemyAI.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    [SerializeField] private Transform currentTargetTransform;
+    [SerializeField] private Vector2 currentTargetPosition = Vector2.zero;
     [SerializeField] private EnemyState currentEnemyState = EnemyState.Idle;
     [SerializeField, Range(1, 10)] private float avoidanceDistance = 4;
     [SerializeField] private LayerMask avoidanceLayerMask = new LayerMask();
 
+    private ShipController shipController;
     private bool isColliding;
     private Vector2 collidingPosition;
     private Vector2 collidingNormal;
@@ -19,6 +22,47 @@ public class EnemyAI : MonoBehaviour
         CollisionAvoiding,
         Chasing,
         Flee
+    }
+
+    private void Awake()
+    {
+        // pathfinding = new Pathfinding();
+        shipController = GetComponent<ShipController>();
+    }
+
+    void Update()
+    {
+        currentTargetPosition = (Vector2)transform.position * transform.up * avoidanceDistance;
+
+        if (currentTargetPosition == Vector2.zero)
+            return;
+        
+        Vector3 direction = transform.InverseTransformDirection(currentTargetPosition - (Vector2)transform.position);
+        float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        float rotValue = 0;
+        if (angle > 10)
+        {
+            rotValue = Mathf.InverseLerp(10, 45, angle);
+            if (shipController.GetForwardVelocity() > (shipController.GetMaxAccelerationSpeed() * 0.5f))
+            {
+                shipController.SetForwardInput(-Mathf.InverseLerp(1, 180, angle));
+            }
+            else shipController.SetForwardInput(1);
+        }
+        else if (angle < -10)
+        {
+            rotValue = -Mathf.InverseLerp(-10, -45, angle);
+            if (shipController.GetForwardVelocity() > (shipController.GetMaxAccelerationSpeed() * 0.5f))
+            {
+                shipController.SetForwardInput(-Mathf.InverseLerp(-1, -180, angle));
+            }
+            else shipController.SetForwardInput(1);
+        }
+        else
+        {
+            shipController.SetForwardInput(1);
+        }
+        shipController.SetAngularInput(rotValue);
     }
 
     private void FixedUpdate()
